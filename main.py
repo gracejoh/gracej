fb1f0ae35e29986a04e632900d1c4ea031e96e6e
gracejoh <ganwynj@icloud.com>
2020-07-15 15:00:03 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-p
+name = "Elia Deppe"
+fav_artist = "Earl Sweatshirt"
+vacation = "Nosara"
+hobby = "painting and skate boarding"
+today = "06/20/22"
+
+print("My name is  " , name , "and my favorite artist is " , fav_artist)
+print("I love to go on vacation to", vacation, "and my two main hobbies are", hobby)
+print("today's date is: ", today)
